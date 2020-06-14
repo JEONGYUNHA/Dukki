ed3b5a4bd7008b99f0f80a7a4bbfb728ca56e475
yunji98 <vkfkddlno@naver.com>
2020-06-14 15:47:37 +0900

    project init

---
+++ app/src/main/java/com/example/dukki/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dukki
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+// 처음 시작부분에 애니메이션 액티비티
+class SplashActivity : AppCompatActivity() {
+
+    val SPLASH_TIME_OUT: Long = 2500
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.translate)
+        startImg.startAnimation(animation)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
+
+    }
+}
