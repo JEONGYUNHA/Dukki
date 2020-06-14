45cde444273ea28ba1e03564e9b0d84125a559e0
yunji98 <vkfkddlno@naver.com>
2020-06-14 16:08:14 +0900

    join

---
+++ app/src/main/java/com/example/dukki/JoinActivity.kt
@@ -0,0 +1,29 @@
+package com.example.dukki
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_join.*
+
+// User data를 나타내는 class
+data class User (var name: String, var id: String, var pw: String, var cash: Int)
+
+// 회원가입 Activity
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+        val db = FirebaseFirestore.getInstance()
+        // Join 버튼 눌렀을 시
+        joinBtn.setOnClickListener{
+            val name: String = userName.text.toString()
+            val id: String = userId.text.toString()
+            val pw: String = userPw.text.toString()
+            val user = User(name, id, pw, 0)
+            db.collection("users").document(name).set(user)
+
+            this.finish()
+        }
+    }
+}
